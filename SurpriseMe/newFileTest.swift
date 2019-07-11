eedc762a949d418d3070405ed01ce3401b8128e3
Youval Ella <youvalella@youvals-macbook-pro.local>
2019-07-11 13:04:31 +0300

    test

---
+++ SurpriseMe/newFileTest.swift
@@ -0,0 +1,13 @@
+//
+//  newFileTest.swift
+//  SurpriseMe
+//
+//  Created by Youval Ella on 11/07/2019.
+//  Copyright © 2019 Surprise. All rights reserved.
+//
+
+import Foundation
+
+class newFile{
+    func newFunc(){}
+}
